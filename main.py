693b067437b6460c21234dc2725806fdae7a48ad
hhwk <kripvan@gmail.com>
2024-02-19 01:57:29 +0300

    Fix

--- main.py
+++ main.py
@@ -684,7 +684,7 @@ if menu=='Посещения':
     if int(db_content[len(db_content)-1]['key'])>len(db_content):
         for p in range(0, len(db_content)-1):
             pp=db_content[p]['key']
-            request.update({'key': str(int(pp)-1)},'5')
+            request.update({'key': str(int(pp)-1)},'3')
     for slash in range(0, len(db_content)):
         st.write(db_content[slash])
         #st.write(db_content[slash]['key'])
